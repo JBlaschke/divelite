1e0755ee636c1dd4658e714383d6bc4a674cd142
Johannes Blaschke <jpblaschke@lbl.gov>
2020-04-15 17:20:08 -0700

    fix post_extend script

--- xtc1to2/post_extend.sh
+++ xtc1to2/post_extend.sh
@@ -4,8 +4,8 @@ mkdir tmp
 mv junk.xtc2 tmp/
 
 pushd tmp
-
-smdwriter -f junk.xtc2 -o data-r0001-s00.smd.xtc2
+mv junk.xtc2  data-r0001-s00.xtc2
+smdwriter -f data-r0001-s00.xtc2 -o data-r0001-s00.smd.xtc2
 mkdir smalldata
 mv *.smd.xtc2 smalldata/
 
